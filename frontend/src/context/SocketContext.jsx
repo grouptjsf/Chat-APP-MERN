d3ac69ec60d54c849359ef2ee22c3fff13fa60e1
grouptjsf <grouptjsf@gmail.com>
2024-02-12 16:53:51 +0100

    [EDIT] Modify the Render URL to match with the Group tjsf account

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -17,6 +17,7 @@ export const SocketContextProvider = ({ children }) => {
 	useEffect(() => {
 		if (authUser) {
 			const socket = io("https://chatapp-mern-o77k.onrender.com", {
+				
 				query: {
 					userId: authUser._id,
 				},
